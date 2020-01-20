e62a2b8b15a8c24edf8179ba6d7270c66eca9dfe
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-01-20 11:14:18 -0500

    updating from local

--- .vscode/test_pytest.py
+++
@@ -1,6 +0,0 @@
-import Rock_Paper_Scissors_Functional
-
-def test_determine_winner (Rock_Paper_Scissors_Functional):
-    def test_determine_winner():
-        assert Rock_Paper_Scissors_Functional.test_determine_winner (rock,paper) = play_again = True
-        
---
+++ __pycache__/test_pytest.py
@@ -0,0 +1,4 @@
+#import Rock_Paper_Scissors_Functional
+
+def test_determine_winner ():
+    assert True
