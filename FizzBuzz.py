3aabcbce75dc75a395d210368c21a3aceda7c203
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-01-27 10:43:14 -0500

    init commit

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+#defining variables
+length_of_round = -1
+
+#replace values divideable by 
+fizz = 3  
+buzz = 5
+fizzbuzz = fizz * buzz 
+
+
+
+def run_round()
+    for i in length_of_round
+
