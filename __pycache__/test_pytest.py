6e9532a09660b845c26b408fad7ad431f2e91e55
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-01-27 10:46:44 -0500

    updating pytest structure

--- __pycache__/test_pytest.py
+++ __pycache__/test_pytest.py
@@ -1,4 +1,14 @@
-#import Rock_Paper_Scissors_Functional
+import sys
+import pytest
+
+sys.path.append('../')
+
+from Rock_Paper_Scissors_Functional import determine_winner
 
 def test_determine_winner ():
-    assert True
+
+    #determine = Rock_Paper_Scissors_Functional()
+
+    #result = determine.determine_winner ("rock","paper")
+
+    assert determine_winner("rock", "paper") == True 
