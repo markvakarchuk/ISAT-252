324b528104e93b930b32b449947d482ead02032c
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-01-27 12:00:58 -0500

    first running version

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,31 @@
-import time
-import random
+import timeit 
 
 #defining variables
-length_of_round = 1
+length_of_round = 00
 
-#replace values divideable by 
-fizz = 3  
-buzz = 5
-fizzbuzz = fizz * buzz 
+#replace values divideable by +
+fizz = 0  
+buzz = 0
+fizzbuzz = 0
 
+length_of_round = int(input("\nEnter length of round desired: "))
+fizz = int(input("Enter divider to replace with 'Fizz': "))
+buzz = int(input("Enter divider to replace with 'Buzz': "))
+fizzbuzz = fizz * buzz
 
+def run_round(i):
+    global fizz
+    global buzz
+    global fizzbuzz
 
-def run_round():
-    for i in length_of_round:
+    if (i % fizzbuzz) == 0:
+        print("FizzBuzz")
+    elif (i % fizz ) == 0:
+        print("Fizz")
+    elif (i % buzz ) == 0:
+        print("Buzz")
+    else:
+        print(i)
 
+for i in range(length_of_round):
+    run_round(i+1)
