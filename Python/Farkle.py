01e9438b569febfa7f06aa601233face795be983
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-01-29 12:31:04 -0500

    added py folder

---
+++ Python/Farkle.py
@@ -0,0 +1,32 @@
+import time, sys
+from random import randint
+
+points = 0
+points_to_win = 4000
+
+number_of_dice = 6
+
+dice_in_round = [0,0,0,0,0,0]
+
+class round():
+    def __init__(self, id, comp_move, user_move):
+        self.id = id
+        self.comp = comp_move
+        self.user = user_move
+
+def roll ():
+    value = randint(0,6) + 1
+    return value
+
+
+
+def go_round (rolls, ):
+    for i in len(rolls)
+        dice= roll()
+        dice
+
+
+def count_points():
+    pass
+
+print(roll())
---
+++ Python/helloworld.py
@@ -0,0 +1,1 @@
+print("\n\nhello world\n\n")
