7e5c73c7360d0c1a8ac8c45b451439db490645e0
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-02-03 10:54:23 -0500

    update git

--- Python/Farkle.py
+++ Python/Farkle.py
@@ -8,22 +8,22 @@ number_of_dice = 6
 
 dice_in_round = [0,0,0,0,0,0]
 
-class round():
-    def __init__(self, id, comp_move, user_move):
-        self.id = id
-        self.comp = comp_move
-        self.user = user_move
+#class round():
+#    def __init__(self, id, comp_move, user_move):
+#       self.id = id
+#        self.comp = comp_move
+#        self.user = user_move
 
 def roll ():
-    value = randint(0,6) + 1
+    value = randint(1,6)
     return value
 
 
 
-def go_round (rolls, ):
-    for i in len(rolls)
-        dice= roll()
-        dice
+#def go_round (rolls, ):
+#    for i in len(rolls)
+#        dice= roll()
+#        dice
 
 
 def count_points():
