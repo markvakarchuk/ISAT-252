8fbfcac756dc1a04302845c1e995c69ce6e519ae
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-02-24 10:25:52 -0500

    switch run_round() from print to reture output

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -1,3 +1,5 @@
+import time
+
 #defining variables
 length_of_round = 00
 
@@ -6,25 +8,41 @@ fizz = 0
 buzz = 0
 fizzbuzz = 0
 
-length_of_round = int(input("\nEnter length of round desired: "))
-print("\nNote: FizzBuzz = Fizz * Buzz")
-fizz = int(input("Enter divider to replace with 'Fizz': "))
-buzz = int(input("Enter divider to replace with 'Buzz': "))
+# length_of_round = int(input("\nEnter length of round desired: "))
+# print("\nNote: FizzBuzz = Fizz * Buzz")
+# fizz = int(input("Enter divider to replace with 'Fizz': "))
+# buzz = int(input("Enter divider to replace with 'Buzz': "))
+
+
+length_of_round = 100
+fizz = 3
+buzz = 5
 fizzbuzz = fizz * buzz
+
+start = time.time()
 
 def run_round(i):
     global fizz
     global buzz
     global fizzbuzz
+    
+    output = ""
 
     if (i % fizzbuzz) == 0:
-        print("FizzBuzz")
+        output = "FizzBuzz"
     elif (i % fizz ) == 0:
-        print("Fizz")
+        output = "Fizz"
     elif (i % buzz ) == 0:
-        print("Buzz")
+        output = "Buzz"
     else:
-        print(i)
+        output = str(i)
+
+    return output
 
 for i in range(length_of_round):
-    run_round(i+1)
+    print(run_round(i+1))
+
+end = time.time()
+
+print("\nHow long program ran(seconds):")
+print(end - start)
