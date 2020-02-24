d9374a9b7da88821017c9e872d30c2c332ddd7c5
markvakarchuk <44476510+markvakarchuk@users.noreply.github.com>
2020-02-24 10:28:02 -0500

    init commit for prog gramming fundmentals series, all 3 parts

---
+++ Python/Programming Fundamental Series.py
@@ -0,0 +1,45 @@
+"""for symbol badge"""
+print("\nThings for symbol badge")
+int = 123     # integer variable 
+str = "abc"   # string variable 
+bool = True   # boolean variable
+flt = 12.3    # floating variable 
+cmp = 12j     # complex variable
+N = None      # null variable
+l = [1, 2, 3] # list(array) variable
+t = (1, 2, 3) # tuple variable
+s = {1, 2, 3} # set variable 
+
+#switch int to a float
+int = 123.4
+
+# print all variable to see update 
+print(int)
+print(str)
+print(bool)
+print(flt)
+print(cmp)
+print(N)
+print(l)
+print(t)
+print(s)
+
+
+"""for operator badge"""
+print("\n\nThings for operator badge")
+intA = 1
+intB = 2
+intC = 3
+strA = "1"
+
+A = intB + intC     # intA + intB gives 5
+B = A > intB        # checks if 5 greater than 2
+C = 3 % 2           # 3 divded by 2 gives remainder of 1
+
+print(A)
+print(B)
+print(C)
+
+
+"""asfd"""
+print("")
